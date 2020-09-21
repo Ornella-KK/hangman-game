71e99fb2ee6419e98740a0f06f23a8e4aee3835b
Ornella-KK <ornellakankazi@gmail.com>
2020-09-21 05:04:32 +0200

    add progress

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var words=["javascript","monkey","amazing","pancake"];
 console.log(words);
 var item=words[Math.floor(Math.random()*words.length)];
+var prog=0;
+while(prog<=0){
+  console.log("You chose a word");
+  prog++;
+}
